56e0fd8125a29ac94bb486493d77b72cf539b1cf
wende363 <wende363@gmail.com>
2022-06-03 07:26:06 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+/**
+ *0-positive_or_negative.c determines if the number stored
+ *in variable n is positive, negative or zero
+ *
+ *
+ */
+
+int main (void)
+{
+int n;
+if (n<0)
+	printf(n, "is negative");
+else if (n>0)
+	printf(n, "is positive");
+else 
+	printf(n, "is zero");		
+		
+return(0);
+}
